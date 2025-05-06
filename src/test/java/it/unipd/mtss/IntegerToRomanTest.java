7a3ca9ea665ff15b59c540126708c22d30012e9c
Alessandro <alessandrofrison04@gmail.com>
2025-05-06 15:36:12 +0200

    Implementata conversione e stampa dei primi 3 numeri, compresi relativi test close #5

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -1,1 +1,20 @@
+//////////////////////////////////////////////////////////////////
+// Alessandro Frison 2111932
+// Riccardo Graziani 2101054
+//////////////////////////////////////////////////////////////////
 
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntegerToRomanTest {
+
+  @Test
+  public void testConversionArabToRoman() {
+    assertEquals("I", IntegerToRoman.convert(1));
+    assertEquals("II", IntegerToRoman.convert(2));
+    assertEquals("III", IntegerToRoman.convert(3));
+  }
+}
